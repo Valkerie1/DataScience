23b867837b4f288d3198a47762ea99ec2f40bd88
Valkerie1 <91312670+Valkerie1@users.noreply.github.com>
2021-09-28 13:50:32 +0200

    Update DataScience-App.py

--- DataScience-App.py
+++ DataScience-App.py
@@ -9,13 +9,15 @@ import time
 
 st.title('Australian passenger information')
 
+with st.spinner('loading...'):
+  time.sleep(5)
+  
 my_bar = st.progress(0)
 for percent_complete in range(100):
   time.sleep(0.1)
   my_bar.progress(percent_complete +1)
 
-with st.spinner('loading...'):
-  time.sleep(1)
+
 st.balloons()
 
 
