9a65658da428463fa94b811a3723ada0c82b14a1
Valkerie1 <91312670+Valkerie1@users.noreply.github.com>
2021-09-28 16:58:39 +0200

    Update DataScience-App.py

--- DataScience-App.py
+++ DataScience-App.py
@@ -274,7 +274,7 @@ for airport in airports:
 
 fig.update_layout(title_text="Passenger growth Australian airports",
             xaxis_title='Years',
-            yaxis_title='Total passengers', width=900, height=700)
+            yaxis_title='Total passengers', width=900, height=500)
 
 
 
