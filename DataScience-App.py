a61cef743e60395adc422749074f52f5a07638c8
Valkerie1 <91312670+Valkerie1@users.noreply.github.com>
2021-09-28 10:24:03 +0200

    Update DataScience-App.py

--- DataScience-App.py
+++ DataScience-App.py
@@ -1,16 +1,16 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import requests
+import json
 
-st.write("""
-# Simple test text
+st.title('Australian passenger information')
 
-underline test text
-
-""")
-
-
-
-
+url = 'https://data.gov.au/data/api/3/action/datastore_search?&resource_id=38bdc971-cb22-4894-b19a-814afc4e8164'
+r=requests.get(url)
+datatxt= r.text
+datajs = json.loads(datatxt)
+datalist = datajs['result']['records']
+df = pd.DataFrame(datalist)
 
 
