671b0e39090205521f2b289687eeacafca0cb218
Valkerie1 <91312670+Valkerie1@users.noreply.github.com>
2021-09-28 13:43:29 +0200

    Update DataScience-App.py

--- DataScience-App.py
+++ DataScience-App.py
@@ -8,6 +8,10 @@ import plotly.graph_objects as go
 
 st.title('Australian passenger information')
 
+add_selectbox = st.sidebar.selectbox('test',('1','2','3'))
+
+
+'''
 url1= 'https://data.gov.au/data/dataset/cc5d888f-5850-47f3-815d-08289b22f5a8/resource/38bdc971-cb22-4894-b19a-814afc4e8164/download/mon_pax_web.csv'  
 url2= 'https://data.gov.au/data/dataset/cc5d888f-5850-47f3-815d-08289b22f5a8/resource/583be26d-59b9-4bcc-827d-4d9f7162fb04/download/mon_acm_web.csv'
 passengers= pd.read_csv(url1)
@@ -28,3 +32,4 @@ fig= px.scatter(data_frame= df, title= 'Plot',
                y= 'Acm_Total',
                color= 'AIRPORT')
 st.plotly_chart(fig)
+'''
