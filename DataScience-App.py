6277b112834735bd2c8848f228cd5b8944ac09cb
Valkerie1 <91312670+Valkerie1@users.noreply.github.com>
2021-09-28 09:47:36 +0200

    Add files via upload

---
+++ DataScience-App.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pip install streamlit
+
+
+# In[3]:
+
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+
+# In[4]:
+
+
+st.write("""
+# Simple test text
+
+underline test text
+
+""")
+
+
+# In[ ]:
+
+
+
+
