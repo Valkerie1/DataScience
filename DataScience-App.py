28ca2f8d6fa0afa410d745ce1bbe1f7758510028
Valkerie1 <91312670+Valkerie1@users.noreply.github.com>
2021-09-28 17:56:52 +0200

    Update DataScience-App.py

--- DataScience-App.py
+++ DataScience-App.py
@@ -308,7 +308,7 @@ fig3.add_trace(go.Bar(x=airports,
 fig3.add_trace(go.Bar(x=airports,
                 y= df_month12.groupby('AIRPORT')['Dom_Pax_Total'].sum() ,
                 name ='Domestic passengers',
-                marker_color='rgb(26, 118, 255)')
+                marker_color='rgb(26, 118, 255)'))
 
 fig3.update_layout(title_text="Domestic and international passengers per airport",
             xaxis_tickfont_size=14,
