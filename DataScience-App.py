b8de813e2b5dc1a1af9873ddfad264a454c9465f
Valkerie1 <91312670+Valkerie1@users.noreply.github.com>
2021-09-28 14:19:44 +0200

    Update DataScience-App.py

--- DataScience-App.py
+++ DataScience-App.py
@@ -12,12 +12,12 @@ st.text('test')
 
 
 with st.expander("See explanation"):
-...     st.write("""
-...         The chart above shows some numbers I picked for you.
-...         I rolled actual dice for these, so they're *guaranteed* to
-...         be random.
-...     """)
-...     st.image("https://static.streamlit.io/examples/dice.jpg")
+     st.write("""
+         The chart above shows some numbers I picked for you.
+         I rolled actual dice for these, so they're *guaranteed* to
+         be random.
+     """)
+    st.image("https://static.streamlit.io/examples/dice.jpg")
 
 '''  
 my_bar = st.progress(0)
