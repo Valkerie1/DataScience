ad193f3a74e634e58b95b3e8082bfc1cb754e791
Valkerie1 <91312670+Valkerie1@users.noreply.github.com>
2021-09-28 09:57:18 +0200

    Update DataScience-App.py

--- DataScience-App.py
+++ DataScience-App.py
@@ -1,22 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-pip install streamlit
-
-
-# In[3]:
-
-
 import streamlit as st
 import numpy as np
 import pandas as pd
-
-
-# In[4]:
-
 
 st.write("""
 # Simple test text
@@ -26,7 +10,6 @@ underline test text
 """)
 
 
-# In[ ]:
 
 
 
