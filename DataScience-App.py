59a43cee0bd1c51b4f400395958c3bf5e12bade1
Valkerie1 <91312670+Valkerie1@users.noreply.github.com>
2021-09-29 14:54:57 +0200

    Update DataScience-App.py

--- DataScience-App.py
+++ DataScience-App.py
@@ -595,10 +595,6 @@ option = st.radio('Select a graph:',
 
 
 if option=='Passenger graph':
-            my_bar = st.progress(0)
-            for percent_complete in range(100):
-                        time.sleep(0.1)
-                        my_bar.progress(percent_complete +1)
             st.plotly_chart(fig5)
 elif option=='Acm graph':
             st.plotly_chart(fig6)
