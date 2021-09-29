78a4a4cd70f68eac39ce7ecb1b44532dc982c23c
Valkerie1 <91312670+Valkerie1@users.noreply.github.com>
2021-09-29 12:18:21 +0200

    Update DataScience-App.py

--- DataScience-App.py
+++ DataScience-App.py
@@ -571,7 +571,7 @@ st.write("""
 """)
 All= df.query('AIRPORT == "All Australian Airports" & Year < 2020')
 
-fig5 = px.scatter(data_frame= All, x= 'Date', y= 'Pax_Total', trendline='lowess')
+fig5 = px.scatter(data_frame= All, x= 'Date', y= 'Pax_Total', trendline='lowess', trendlineoptions=dict(frac=0.1))
 st.plotly_chart(fig5)
 
 
